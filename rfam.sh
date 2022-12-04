76f4c5bf55499548aba9c56474780324f78e45ba
Nataly <88973443+natalyevpak@users.noreply.github.com>
2022-12-04 14:17:51 +0300

    Update rfam.sh

--- rfam.sh
+++ rfam.sh
@@ -1,7 +1,6 @@
-mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam -e
-"SELECT fr.rfam_acc, fr.rfamseq_acc, fr.seq_start, fr.seq_end
+SELECT fr.rfam_acc, fr.rfamseq_acc, fr.seq_start, fr.seq_end
 FROM full_region fr, rfamseq rf, taxonomy tx
 WHERE rf.ncbi_id = tx.ncbi_id
 AND fr.rfamseq_acc = rf.rfamseq_acc
 AND tx.ncbi_id = 10116 -- NCBI taxonomy id of Rattus norvegicus
-AND is_significant = 1 -- exclude low-scoring matches from the same clan"
+AND is_significant = 1 -- exclude low-scoring matches from the same clan
